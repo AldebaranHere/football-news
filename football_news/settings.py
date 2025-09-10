e80e2b0cd194f9dae880edbbb737e771c9cb1be0
Aldebaran64 <aldebaran26adhitya@gmail.com>
2025-09-10 17:30:11 +0700

    Fixing CSRF TRUSTED ORIGINS

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,8 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/aldebaran.rahman/footballnews", "aldebaran-rahman-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://aldebaran-rahman-footballnews.pbp.cs.ui.ac.id/",
-    "https://pbp.cs.ui.ac.id/aldebaran.rahman/footballnews",
+    "https://aldebaran-rahman-footballnews.pbp.cs.ui.ac.id",
 ]
 
 # Application definition
