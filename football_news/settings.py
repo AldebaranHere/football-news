538ec5c564215067c75ac9112fe5c0032597f6bd
Aldebaran64 <aldebaran26adhitya@gmail.com>
2025-09-10 14:51:26 +0700

    Tutorial 3 - near the end

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,6 +33,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/aldebaran.rahman/footballnews", "aldebaran-rahman-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://aldebaran-rahman-footballnews.pbp.cs.ui.ac.id/"
+]
 
 # Application definition
 
@@ -61,7 +64,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
